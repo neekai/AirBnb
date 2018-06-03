b07b7b067d9dbbff63b2c91227545f8ea6df90c9
Nick Cai <Nickcai3339@gmail.com>
2018-06-02 18:31:44 -0700

    renders description, show more and hide for description works

--- client/components/Description.jsx
+++ client/components/Description.jsx
@@ -1,19 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+// import ListingDescription from './Style.jsx';
+
+const ListingDescription = styled.div`
+  font-size: 16px;
+  font-family: 'Source Sans Pro', sans-serif;
+  font-weight: 400;
+`;
+
+const ReadMoreAndHide = styled.a`
+font-size: 16px;
+font-family: 'Source Sans Pro', sans-serif;
+font-weight: 600;
+color: #008489
+`;
 
 class Description extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showText: false
+            expanded: false,
+            description: ''
         }
     }
 
+    expandedText() {
+      this.setState({
+        expanded: true
+      });
+    }
+
+    collapseText() {
+      this.setState({
+        expanded: false
+      });
+    }
+
+    
+
 
     render() {
+        let firstLine = this.props.listingInfo.description;
+        let rest = this.props.listingInfo.description;
+        // firstLine = firstLine.split('.');
         return (
-            <div id="listing_description">
-                {this.props.listingInfo.description}
-            </div>
+          <div id="listing_description">
+            <ListingDescription>
+              { firstLine !== undefined ? firstLine.split('.')[0] : null }
+            </ListingDescription>
+              <div>
+                { !this.state.expanded ? <a onClick={() => {this.expandedText()}}><ReadMoreAndHide>Read more about the space</ReadMoreAndHide></a> : null }
+                  { this.state.expanded ? <ListingDescription>
+                  { rest !== undefined ? rest.split('.').slice(1).join('.') : null }
+                </ListingDescription> : null }
+                { this.state.expanded ? <a onClick={() => {this.collapseText()}}><ReadMoreAndHide>Hide</ReadMoreAndHide></a> : null }
+              </div>
+          </div>
         )
     }
 }
--- client/components/Style.jsx
+++ client/components/Style.jsx
@@ -1,2 +1,14 @@
-import styled from 'styled-components';
+// import React from 'react';
+// import styled from 'styled-components';
 
+// const ListingDescription = styled.div`
+//   font-size: 16px;
+//   font-family: 'Source Sans Pro', sans-serif;
+//   font-weight: 400;
+// `;
+
+// const OutsetBox = () => {
+
+// }
+
+// export default Style;
